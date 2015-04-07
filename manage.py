02c2767a32d88aa2499e76ba618c5204113157ab
sinwoobang <sinwoobang@gmail.com>
2015-04-07 20:50:39 +0900

    Create manage_script...
    - It works like flask-script.

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 # -*-coding:utf-8-*-
+import sys
+
 from app import create_api, Base, engine
 
 from wsgiref import simple_server
@@ -6,7 +8,21 @@ from wsgiref import simple_server
 
 api = create_api()
 
+
+def manage_script(argv):
+    args = argv[1:] if len(argv) > 1 else None
+    if not args:
+        sys.exit()
+    elif args[0] == 'initdb':
+        print 'Falcon : Create the metadata of Base.'
+        Base.metadata.create_all(bind=engine)
+
+    elif args[0] == 'runserver':
+        from datetime import datetime
+        print '** Falcon Works {0} **'.format(str(datetime.now())[:19])
+        httpd = simple_server.make_server('127.0.0.1', 8000, api)
+        httpd.serve_forever()
+
+
 if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
-    httpd = simple_server.make_server('127.0.0.1', 8000, api)
-    httpd.serve_forever()
+    manage_script(sys.argv)
