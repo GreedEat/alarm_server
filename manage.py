42bb6e4091453caa48e4ad65fa7d0b6560d7aa01
sinwoobang <sinwoobang@gmail.com>
2015-05-12 17:05:13 +0900

    Delete sqlalchemy & db.

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 # -*-coding:utf-8-*-
 import sys
 
-from app import create_api, Base, engine
+from app import create_api
 
 from wsgiref import simple_server
 
@@ -13,13 +13,10 @@ def manage_script(argv):
     args = argv[1:] if len(argv) > 1 else None
     if not args:
         sys.exit()
-    elif args[0] == 'initdb':
-        print 'Falcon : Create the metadata of Base.'
-        Base.metadata.create_all(bind=engine)
     elif args[0] == 'runserver':
         from datetime import datetime
         print '** Falcon Works {0} **'.format(str(datetime.now())[:19])
-        httpd = simple_server.make_server('127.0.0.1', 8000, api)
+        httpd = simple_server.make_server('0.0.0.0', 8000, api)
         httpd.serve_forever()
 
 
